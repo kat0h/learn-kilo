44837ff7f5a4ef29b470db2bba88eea3561c9b59
KotaKato <peony.btn@gmail.com>
2021-01-12 22:37:38 +0900

    終了時にrawモードを無効化する

---
+++ kilo.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+// orig_termiosは元の状態
+struct termios orig_termios;
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+    struct termios raw;
+    atexit(disableRawMode);
+
+    tcgetattr(STDIN_FILENO, &raw);
+    orig_termios = raw;
+    raw.c_lflag &= ~(ECHO);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main() {
+    enableRawMode();
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    return 0;
+}
