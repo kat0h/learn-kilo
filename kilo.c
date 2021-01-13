fc83f27cb78872a2832da3e5b410465331ee65cc
KotaKato <peony.btn@gmail.com>
2021-01-13 23:14:06 +0900

    Add some flags for raw mode

--- kilo.c
+++ kilo.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <sys/termios.h>
 #include <termios.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -17,8 +18,10 @@ void enableRawMode() {
 
     tcgetattr(STDIN_FILENO, &raw);
     orig_termios = raw;
-    raw.c_iflag &= ~(IXON);
-    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
