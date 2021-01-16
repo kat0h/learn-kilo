089310279633de19636b8840f24acc2192bff8c5
KotaKato <peony.btn@gmail.com>
2021-01-16 22:39:20 +0900

    erase line

--- kilo.c
+++ kilo.c
@@ -82,6 +82,8 @@ void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
     abAppend(ab, "~", 1);
+
+    abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows -1) {
       abAppend(ab, "\r\n", 2);
     }
@@ -90,12 +92,15 @@ void editorDrawRows(struct abuf *ab) {
 
 void editorRefreshScreen() {
   struct abuf ab = ABUF_INIT;
-  abAppend(&ab, "\x1b[2J", 4);
+
+  // hide cursor
+  abAppend(&ab, "\x1b[?25l", 6);
   abAppend(&ab, "\x1b[H", 3);
 
   editorDrawRows(&ab);
 
   abAppend(&ab, "\x1b[H", 3);
+  abAppend(&ab, "\x1b[?25h", 6);
 
   write(STDOUT_FILENO, ab.b, ab.len);
   abFree(&ab);
