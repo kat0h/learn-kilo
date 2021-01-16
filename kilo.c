dd54c93b36fab314a10b829f88bddafec99e185d
KotaKato <peony.btn@gmail.com>
2021-01-16 21:37:28 +0900

    get window size

--- kilo.c
+++ kilo.c
@@ -9,7 +9,18 @@
 #include <termios.h>
 #include <unistd.h>
 
+
+// k -> <C-k>
 #define CTRL_KEY(k) ((k)&0x1f)
+
+// エディタの設定
+struct editorConfig {
+  int screenrows;
+  int screencols;
+  struct termios orig_termios;
+};
+struct editorConfig E;
+
 
 void die(const char *s) {
   write(STDOUT_FILENO, "\x1b[2J", 4);
@@ -18,28 +29,6 @@ void die(const char *s) {
   exit(1);
 }
 
-struct editorConfig {
-  int screenrows;
-  int screencols;
-  struct termios orig_termios;
-};
-
-int getWindowSize(int *rows, int *cols) {
-  struct winsize ws;
-  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-    return -1;
-  } else {
-    *rows = ws.ws_row;
-    *cols = ws.ws_col;
-    return 0;
-  }
-}
-
-struct editorConfig E;
-
-void initEditor() {
-  if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
-}
 
 void disableRawMode() {
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
@@ -73,9 +62,10 @@ char editorReadKey() {
 
 void editorDrawRows() {
   int y;
-  for (y = 0; y < E.screenrows; y++) {
+  for (y = 0; y < E.screenrows -1; y++) {
     write(STDOUT_FILENO, "~\r\n", 3);
   }
+    write(STDOUT_FILENO, "~", 2);
 }
 
 void editorRefreshScreen() {
@@ -97,6 +87,42 @@ void editorProcessKeyPress() {
     exit(0);
     break;
   }
+}
+
+int getCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+
+  if(write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+  while (i<sizeof(buf) -1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+    if (buf[i] == 'R') break;
+    i++;
+  }
+  buf[i] = '\0';
+
+  if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)return -1;
+
+  return 0;
+}
+
+int getWindowSize(int *rows, int *cols) {
+  struct winsize ws;
+  // Debug用に1||
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    if (write(STDOUT_FILENO, "\x1b[9999C\x1b[999B", 14) != 14) return -1;
+    return getCursorPosition(rows, cols);
+  } else {
+    *rows = ws.ws_row;
+    *cols = ws.ws_col;
+    return 0;
+  }
+}
+
+void initEditor() {
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1)
+    die("getWindowSize");
 }
 
 int main() {
