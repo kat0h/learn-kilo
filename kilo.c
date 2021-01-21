15edfae29dbb4669e8468c58077cc271b85d4afd
KotaKato <peony.btn@gmail.com>
2021-01-21 23:21:05 +0900

    move cursor with Page Up & Page Down Keys

--- kilo.c
+++ kilo.c
@@ -24,7 +24,14 @@ struct editorConfig {
 };
 struct editorConfig E;
 
-enum editorKey { ARROW_LEFT = 1000, ARROW_RIGHT, ARROW_UP, ARROW_DOWN };
+enum editorKey {
+  ARROW_LEFT = 1000,
+  ARROW_RIGHT,
+  ARROW_UP,
+  ARROW_DOWN,
+  PAGE_UP,
+  PAGE_DOWN
+};
 
 struct abuf {
   char *b;
@@ -90,25 +97,35 @@ int editorReadKey() {
       return '\x1b';
 
     if (seq[0] == '[') {
-      switch (seq[1]) {
-      case 'A':
-        return ARROW_UP;
-      case 'B':
-        return ARROW_DOWN;
-      case 'C':
-        return ARROW_LEFT;
-      case 'D':
-        return ARROW_RIGHT;
+      if (seq[1] >= '0' && seq[1] <= '9') {
+        if (read(STDIN_FILENO, &seq[2], 1) != 1)
+          return '\x1b';
+        if (seq[2] == '~') {
+          switch (seq[1]) {
+          case '5':
+            return PAGE_UP;
+          case '6':
+            return PAGE_DOWN;
+          }
+        }
+      } else {
+        switch (seq[1]) {
+        case 'A':
+          return ARROW_UP;
+        case 'B':
+          return ARROW_DOWN;
+        case 'C':
+          return ARROW_RIGHT;
+        case 'D':
+          return ARROW_LEFT;
+        }
       }
-
-      return '\x1b';
-
-    } else {
-      return c;
     }
-  }
 
-  return c;
+    return '\x1b';
+  } else {
+    return c;
+  }
 }
 
 void editorDrawRows(struct abuf *ab) {
@@ -160,19 +177,19 @@ void editorRefreshScreen() {
 
 void editorMoveCursor(int key) {
   switch (key) {
-  case ARROW_RIGHT:
-    if (E.cx !=  0) {
+  case ARROW_LEFT:
+    if (E.cx != 0) {
       E.cx--;
     }
     break;
-  case ARROW_LEFT:
+  case ARROW_RIGHT:
     if (E.cx != E.screencols - 1) {
       E.cx++;
     }
-    break;
 
+    break;
   case ARROW_UP:
-    if (E.cy !=  0) {
+    if (E.cy != 0) {
       E.cy--;
     }
     break;
@@ -194,6 +211,13 @@ void editorProcessKeyPress() {
     exit(0);
     break;
 
+  case PAGE_UP:
+  case PAGE_DOWN: {
+    int times = E.screenrows;
+    while (times--) {
+      editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+    }
+  } break;
   case ARROW_UP:
   case ARROW_DOWN:
   case ARROW_LEFT:
